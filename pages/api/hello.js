048f7dcfc3ecbce539827abd6d84ac6926be6d63
zisko <jacobmizraji@gmail.com>
2021-04-24 15:42:16 +0000

    add systeminfo

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const si = require('systeminformation');
 
 export default (req, res) => {
+  si.cpu()
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
   res.status(200).json({ name: 'John Doe' })
 }
