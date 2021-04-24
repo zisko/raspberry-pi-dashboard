2b4c91f0af975a1412aade1ccb48b1c50473eaeb
zisko <jacobmizraji@gmail.com>
2021-04-24 20:09:14 +0000

    add api route to give console messages

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import * as si from 'systeminformation';
-import {exec} from 'child_process';
+import * as util from 'util';
+
+const exec = util.promisify(require('child_process').exec);
 
 export default async (req, res) => {
-  exec('zisko_chia_show', (err, stdout, stderr) => {
-    let results = { name: stdout };
-    if (stderr) {
-      results['err'] = stderr;
-      res.status(400).json(results);
-      return res.end();
-    }
-    res.status(200).json(results);
+  let chia_show_results = {};
+  let chia_farm_results = {};
+  try{
+    chia_show_results = await exec('/home/ubuntu/repos/linux_utils/chia_status.sh');
+    chia_farm_results = await exec('/home/ubuntu/repos/linux_utils/chia_farm_status.sh');
+  }
+  catch(err){
+    chia_show_results['err'] = err;
+    res.status(400).json({show: chia_show_results, farm: chia_farm_results});
     return res.end();
-  });
+  }
+  if (chia_show_results.stderr) {
+    chia_show_results['err'] = stderr;
+    res.status(400).json({show: chia_show_results, farm: chia_farm_results});
+    return res.end();
+  }
+  res.status(200).json({show: chia_show_results, farm: chia_farm_results});
+  return res.end();
 }
