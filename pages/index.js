562401a7de93d9c0c0fb8ec58c1344c4f97d06e1
zisko <jacobmizraji@gmail.com>
2021-04-25 04:30:01 +0000

    added refresh button

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,21 @@ import React from "react";
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 function ChiaOutput() {
   console.log('woah wtf')
-  const { data, error } = useSWR('/api/hello', fetcher)
+  const { data, mutate, error, isValidating } = useSWR('/api/hello', fetcher)
 
   if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-  return <div style={{whiteSpace: "pre-wrap"}} className="mono">
+  if (isValidating) return <div>loading...</div>
+  return ( <div><button onClick={mutate}>refresh</button><div style={{whiteSpace: "pre-wrap"}} className="mono">
   { data.show } 
   { data.farm}
-  </div>
+  </div></div>)
 }
 
 
 function HomePage() {
   return (
-    <div><ChiaOutput></ChiaOutput></div> 
+    <div>
+    <ChiaOutput></ChiaOutput></div> 
     
   )
 
